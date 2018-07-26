daa7fe5960649ae56e9179055a45c1c61a368286
eslawek <ellieslawek@icloud.com>
2018-07-26 18:33:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,11 @@
 # dog.rb
+
+class Dog
+  def initialize(name)
+    @name = name 
+  end #def initialize
+end #class dogs 
+
+fido = Dog.new("Fido")
+snoopy = Dog.new("Snoopy")
+lassie = Dog.new("Lassie")
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,9 @@
 # person.rb
+
+class Person
+  def intialize(name)
+    @name = name
+end #class Person
+
+adele_goldberg = Person.new("Adele Goldberg")
+alan_key = Person.new("Alan Key")
